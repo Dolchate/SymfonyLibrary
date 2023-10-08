152fd8485807f5fd9b87f071f1ab548e6acaccc4
Dolchate <aurelerasschaert@gmail.com>
2023-10-09 00:06:31 +0200

    Added vignette.html.twig file to represent a readable entity in the home page (only for novels for now)

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Manga;
+use App\Entity\Manhwa;
+use App\Entity\Novel;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('public/index.html.twig', [
-            'controller_name' => 'PublicController',
+
+        $novels = $entityManager->getRepository(Novel::class)->findAll();
+        $mangas = $entityManager->getRepository(Manga::class)->findAll();
+        $manhwas = $entityManager->getRepository(Manhwa::class)->findAll();
+
+        return $this->render('public/index.html.twig',[
+            'novels' => $novels,
+            'mangas' => $mangas,
+            'manhwas' => $manhwas,
         ]);
     }
 }
